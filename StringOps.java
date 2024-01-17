60d9912bcad01912816e51e9528cee56286478dd
Lavie-Zanzuri <154608667+Lavie-Zanzuri@users.noreply.github.com>
2024-01-17 23:13:09 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -27,7 +27,18 @@ public class StringOps {
 
     public static String capVowelsLowRest (String string) {
         // Write your code here:
-        return "";
+        String newst = "";
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            int ascii = (int) ch;
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                ch = (char) (ascii - 32);
+            } else if (ascii >= 65 && ascii <= 90) {
+                ch = (char) (ascii + 32);
+            }
+            newst = newst + ch;
+        }
+        return newst;
     }
 
     public static String camelCase (String string) {
